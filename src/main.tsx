17efe440e537a0b294174af17534daf9068e45c5
yuting4d <113909746+yuting4d@users.noreply.github.com>
2026-03-16 15:21:10 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import CulturalStories from './pages/CulturalStories.tsx';
 import { LanguageProvider } from './LanguageContext.tsx';
@@ -9,12 +9,12 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LanguageProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/cultural-stories" element={<CulturalStories />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </LanguageProvider>
   </StrictMode>
 );
